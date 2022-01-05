5dbf4813618f3e690da5162b96eb6f6f2e3306c6
Leah Ewing <leahewing@live.com>
2022-01-05 18:16:13 +0000

    split_pairs doctests

--- split_pairs.py
+++ split_pairs.py
@@ -8,17 +8,27 @@ the final pair should be replaced with an underscore ('_').
 Input: A string
 Output: An iterable of strings
 
+Precondition: 0 <= len(str) <= 100
+
 >>> split_pairs('abcd')
 ['ab', 'cd']
 
 >>> split_pairs('abc')
 ['ab', 'c_']
 
+>>> list(split_pairs('abcdf'))
+['ab', 'cd', 'f_']
+
+>>> list(split_pairs('a'))
+['a_']
+
+>>> list(split_pairs(''))
+[]
 
 """
 
 def split_pairs(a):
-    
+
     pairs = []
 
     while len(a) > 1:
@@ -28,7 +38,7 @@ def split_pairs(a):
     if len(a) > 0:
         pairs.append(f'{a[0]}_')
 
-    print(pairs)
+    return pairs
 
 
 
