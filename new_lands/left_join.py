55d25af557a1b8386deaf1f452d51482c69d395c
Leah Ewing <leahewing@live.com>
2022-01-08 05:11:06 +0000

    lef_join

--- new_lands/left_join.py
+++ new_lands/left_join.py
@@ -30,17 +30,8 @@ def left_join(phrases):
     new_phrase = ','.join(phrases)
     new_phrase = new_phrase.replace("right", "left")
 
-    # print(new_phrase)
     return new_phrase
-    
 
-
-
-
-# left_join(("left", "right", "left", "stop")) # => "left,left,left,stop"
-# left_join(("bright aright", "ok")) # => "bleft aleft,ok"
-# left_join(("brightness wright",)) # => "bleftness wleft"
-# left_join(("enough", "jokes")) # => "enough,jokes"
 
 
 if __name__ == '__main__':
