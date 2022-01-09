a7dc0fe1fb0300cd741e811c8843de30c6e82504
Leah Ewing <leahewing@live.com>
2022-01-09 05:15:43 +0000

    count_digits.py

---
+++ new_lands/count_digits.py
@@ -0,0 +1,45 @@
+"""
+
+Count the number of digits in a given string.
+
+Input: A Str.
+Output: An Int.
+
+
+>>> count_digits('hi')
+0
+
+>>> count_digits('who is 1st here')
+1
+
+>>> count_digits('my numbers is 2')
+1
+>>> count_digits('This picture is an oil on canvas painting by Danish artist Anna Petersen between 1845 and 1910 year')
+8
+
+>>> count_digits('5 plus 6 is')
+2
+
+>>> count_digits('')
+0
+
+"""
+
+def count_digits(text):
+    numbers_in_text = []
+    digits = '1234567890'
+
+    for letter in text:
+        if letter in digits:
+            numbers_in_text.append(letter)
+    
+    answer = len(numbers_in_text)
+
+    print(answer)
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n🥳 ALL TESTS PASSED! YAY!\n')
