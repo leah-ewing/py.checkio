9b5325be7c0beeedeb8c48db2aac8d12d1f237cb
Leah Ewing <leahewing@live.com>
2022-01-10 03:51:04 +0000

    bigger_price

--- new_lands/bigger_price.py
+++ new_lands/bigger_price.py
@@ -38,27 +38,6 @@ def bigger_price(limit, data):
 
 
 
-
-# 1.
-bigger_price(2, [
-    {"name": "bread", "price": 100},
-    {"name": "wine", "price": 138},
-    {"name": "meat", "price": 15},
-    {"name": "water", "price": 1}
-]) 
-# => [
-#   => {"name": "wine", "price": 138},
-#   => {"name": "bread", "price": 100}
-# => ]
-
-# 2. 
-# bigger_price(1, [
-#     {"name": "pen", "price": 5},
-#     {"name": "whiteboard", "price": 170}
-# ]) 
-# => [{"name": "whiteboard", "price": 170}]
-
-
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
