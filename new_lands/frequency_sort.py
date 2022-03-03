d11d337d94fc4d1c0bd71b209e67377b8ccaacad
Leah Ewing <leahewing@live.com>
2022-03-03 21:37:48 +0000

    updated for added test cases

--- new_lands/frequency_sort.py
+++ new_lands/frequency_sort.py
@@ -13,6 +13,12 @@ Output: Iterable
 >>> frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob'])
 ['bob', 'bob', 'bob', 'carl', 'alex']
 
+>>> frequency_sort([])
+[]
+
+>>> frequency_sort([1])
+[1]
+
 """
 
 
@@ -21,46 +27,50 @@ def frequency_sort(items):
     word_counts = []
     word_list = []
 
-    i = 0
-    while len(items_list) > 1:
-        word_counts.append(1)
-        if items_list[0] == items_list[1]:
-            word_counts[i] += 1
-        else:
+    if sorted(items) == sorted(list(set(items))):
+        print(items)
+        
+    else:
+        i = 0
+        while len(items_list) > 1:
+            word_counts.append(1)
+            if items_list[0] == items_list[1]:
+                word_counts[i] += 1
+            else:
+                i += 1
+                word_list.append(items_list[0])
+
+            items_list.pop(0)
+
+        word_list.append(items_list[0])
+        word_counts = word_counts[0:len(word_list)]
+        words_and_counts = []
+
+        i = 0
+        while i < len(word_list):
+            words_and_counts.append([word_list[i], [word_counts[i]]])
             i += 1
-            word_list.append(items_list[0])
+        
+        word_counts = sorted(word_counts)
+        sorted_words_and_counts = []
 
-        items_list.pop(0)
+        i = 0
+        while i < len(word_counts):
+            for item in words_and_counts:
+                if item[1][0] == word_counts[i] and item not in sorted_words_and_counts:
+                    sorted_words_and_counts.insert(0, item)
+            i += 1
 
-    word_list.append(items_list[0])
-    word_counts = word_counts[0:len(word_list)]
-    words_and_counts = []
+        new_list = []
 
-    i = 0
-    while i < len(word_list):
-        words_and_counts.append([word_list[i], [word_counts[i]]])
-        i += 1
-    
-    word_counts = sorted(word_counts)
-    sorted_words_and_counts = []
+        i = 0
+        while i < len(sorted_words_and_counts):
+            for item in items:
+                if item == sorted_words_and_counts[i][0]:
+                    new_list.append(item)
+            i += 1
 
-    i = 0
-    while i < len(word_counts):
-        for item in words_and_counts:
-            if item[1][0] == word_counts[i] and item not in sorted_words_and_counts:
-                sorted_words_and_counts.insert(0, item)
-        i += 1
-
-    new_list = []
-
-    i = 0
-    while i < len(sorted_words_and_counts):
-        for item in items:
-            if item == sorted_words_and_counts[i][0]:
-                new_list.append(item)
-        i += 1
-
-    print(new_list)
+        print(new_list)
 
 
 
