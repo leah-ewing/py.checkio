2c66d2f6397a2ca7fe21cff788b979d1f18424a8
Leah Ewing <leahewing@live.com>
2022-03-07 04:34:11 +0000

    translate.py

--- scientific_expedition/translate.py
+++ scientific_expedition/translate.py
@@ -1,13 +1,20 @@
 """
 
-Stephan has a friend who happens to be a little mechbird. Recently, he was trying to teach it how to speak basic language. Today the bird spoke its first word: "hieeelalaooo". This sounds a lot like "hello", but with too many vowels. Stephan asked Nikola for help and he helped to examine how the bird changes words. With the information they discovered, we should help them to make a translation module.
+Stephan has a friend who happens to be a little mechbird. 
+Recently, he was trying to teach it how to speak basic language. 
+Today the bird spoke its first word: "hieeelalaooo". 
+This sounds a lot like "hello", but with too many vowels. 
+Stephan asked Nikola for help and he helped to examine how the bird changes words. 
+With the information they discovered, we should help them to make a translation module.
 
 The bird converts words by two rules: 
-- after each consonant letter the bird appends a random vowel letter (l ⇒ la or le);
-- after each vowel letter the bird appends two of the same letter (a ⇒ aaa);
-Vowels letters == "aeiouy".
+- After each consonant letter the bird appends a random vowel letter (l ⇒ la or le);
+- After each vowel letter the bird appends two of the same letter (a ⇒ aaa);
+(Vowels letters == "aeiouy")
 
-You are given an ornithological phrase as several words which are separated by white-spaces (each pair of words by one whitespace). The bird does not know how to punctuate its phrases and only speaks words as letters. All words are given in lowercase. You should translate this phrase from the bird language to something more understandable.
+You are given an ornithological phrase as several words which are separated by white-spaces (each pair of words by one whitespace). 
+The bird does not know how to punctuate its phrases and only speaks words as letters. 
+All words are given in lowercase. You should translate this phrase from the bird language to something more understandable.
 
 Input: A bird phrase as a string.
 Output: The translation as a string.
@@ -31,7 +38,9 @@ z
 >>> translate("aaa")
 a
 
+
 """
+
 
 def translate(text):
 
@@ -67,15 +76,6 @@ def translate(text):
     print(new_list)
 
 
-
-
-
-# # translate('hieeelalaooo') # == 'hello'
-# # translate('hoooowe yyyooouuu duoooiiine') # == 'how you doin'
-# # translate('aaa bo cy da eee fe') # == 'a b c d e f'
-# # translate('sooooso aaaaaaaaa') # == 'sos aaa'
-# translate("aaa") # == a
-# translate('xy') # == x
 
 
 if __name__ == "__main__":
